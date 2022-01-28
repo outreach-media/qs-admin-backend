909b0341aa3d592b75b33dc14ae7a916416ef258
akash-dutta-au7 <dutta.akash1993official@gmail.com>
2022-01-28 13:05:46 +0530

    make it better

--- app.js
+++ app.js
@@ -23,8 +23,14 @@ const contentList = require("./routes/contentRoute");
 //routes
 app.use("/content", contentList);
 //server
-const port = process.env.PORT || 5000;
-const server_host = "0.0.0.0";
-app.listen(port, server_host, () => {
-  console.log(`App is running at port ${port}`);
+// const port = process.env.PORT || 5000;
+// const server_host = "0.0.0.0";
+// app.listen(port, server_host, () => {
+//   console.log(`App is running at port ${port}`);
+// });
+
+var server_port = process.env.YOUR_PORT || process.env.PORT || 5000;
+var server_host = process.env.YOUR_HOST || "0.0.0.0";
+app.listen(server_port, server_host, function () {
+  console.log("Listening on port %d", server_port);
 });
