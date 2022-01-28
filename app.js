772a70bb5192e4febd7d3e6a91a91da9869c1b81
akash-dutta-au7 <dutta.akash1993official@gmail.com>
2022-01-28 13:16:22 +0530

    make it better

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const connectDB = require("./db/db");
 const app = express();
+require("dotenv").config();
 
 //middleware
 app.use(express.json({ extended: false }));
@@ -22,15 +23,10 @@ const contentList = require("./routes/contentRoute");
 
 //routes
 app.use("/content", contentList);
-//server
-// const port = process.env.PORT || 5000;
-// const server_host = "0.0.0.0";
-// app.listen(port, server_host, () => {
-//   console.log(`App is running at port ${port}`);
-// });
 
-var server_port = process.env.YOUR_PORT || process.env.PORT || 5000;
-var server_host = process.env.YOUR_HOST || "0.0.0.0";
-app.listen(server_port, server_host, function () {
-  console.log("Listening on port %d", server_port);
+//server
+const port = process.env.PORT || 5000;
+const host = "0.0.0.0";
+app.listen(port, host, () => {
+  console.log(`App is running at port ${port}`);
 });
